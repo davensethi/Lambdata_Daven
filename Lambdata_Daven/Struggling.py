e3666331384bfa62ca60e157b9759f203706c5e9
davensethi@gmail.com <davensethi@gmail.com>
2020-09-30 11:21:02 -0400

    praying for all this to work

--- Lambdata_Daven/Struggling.py
+++ Lambdata_Daven/Struggling.py
@@ -2,18 +2,39 @@
 
 import pandas as pd 
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 ## df_explorer tries to let you explore your dataset faster
 
+class TheStruggle:
+  def __init__(self, input):
+    """Takes in a DataFrame to explore easily """
+    """Atribute"""
 
-def df_explorer(df):
-  print(df.head(10)),
-  print(df.shape)
-  print(df.info)
+    self.X = input
 
-## df_cleaning helps you quickly git of 
+  def df_explorer(self):
+    print(self.X.head(10)),
+    print(self.X.shape)
+    print(self.X.info)
 
-  def df_no_zero_no_nan(df):
-      df.replace(0, np.nan, inplace= True)
-      df = df.dropna()
-      return df.head(10)
+  ## df_cleaning helps you quickly git of 
+
+    def df_no_zero_no_nan(self):
+        self.X.replace(0, np.nan, inplace= True)
+        self.X.dropna()
+        return self.X.head(10)
+
+class NotSureWorks:
+  def __init__(self, target, df):
+    ## X equals the data frame 
+    ## y equals the targeted vector
+    self.X = df.drop(columns = target, axis=1)
+    self.y = df[target] 
+
+  def NotSure(self):
+    X_train, X_test, y_train, y_test = train_test_split(self.X,self.y, test_size=0.33)
+    print(X_train)
+    print(X_test)
+    print(y_train)
+    print(y_train)
